273b0c4ada9852e73dbe9088b85a8a3d5d23ddac
wonjaelee09 <74937920+wonjaelee09@users.noreply.github.com>
2023-04-06 16:55:04 -0400

    folder name

--- Immediate Food Delivery I.sql
+++ Immediate Food Delivery I.sql
@@ -6,6 +6,3 @@ insert into Delivery (delivery_id, customer_id, order_date, customer_pref_delive
 insert into Delivery (delivery_id, customer_id, order_date, customer_pref_delivery_date) values ('4', '3', '2019-08-24', '2019-08-26');
 insert into Delivery (delivery_id, customer_id, order_date, customer_pref_delivery_date) values ('5', '4', '2019-08-21', '2019-08-22');
 insert into Delivery (delivery_id, customer_id, order_date, customer_pref_delivery_date) values ('6', '2', '2019-08-11', '2019-08-13');
-test
-fe
-
